4fa022f169cdb4bd9f4d68abdf21e4eceac52e34
Rafael Ferraz <rafael.ferraz@live.com>
2023-07-21 00:36:50 -0300

    Conteúdo Aula 03 parcial - criação dos métodos do Service para Person (mockado)

--- JavaCursoInverno/src/main/java/com/java/inverno/controllers/PersonController.java
+++ JavaCursoInverno/src/main/java/com/java/inverno/controllers/PersonController.java
@@ -1,8 +1,15 @@
 package com.java.inverno.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +26,25 @@ public class PersonController {
 	@GetMapping(path = "/{id}")
 	public Person findOne(@PathVariable(name = "id") String id) {
 		return service.findById(id);
+	}
+	
+	@GetMapping()
+	public List<Person> findAll() {
+		return service.findAll();
+	}
+	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) {
+		return service.create(person);
+	}
+	
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) {
+		return service.update(person);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public void delete(@PathVariable (value = "id") String id) {
+		service.delete(id);
 	}
 }
--- JavaCursoInverno/src/main/java/com/java/inverno/services/PersonServices.java
+++ JavaCursoInverno/src/main/java/com/java/inverno/services/PersonServices.java
@@ -1,5 +1,7 @@
 package com.java.inverno.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -24,4 +26,39 @@ public class PersonServices {
 		return person;
 	}
 	
+	public List<Person> findAll() {
+		logger.info("Buscando todas as pessoas");
+		List<Person> persons = new ArrayList<>();
+		
+		for (int i = 0; i < 10; i++) {
+			var person = mockPerson(i);
+			persons.add(person);
+		}
+		
+		return persons;
+	}
+	
+	public Person create(Person person) {
+		logger.info("Criando uma pessoa");
+		return person;
+	}
+	
+	public Person update(Person person) {
+		logger.info("Atualizando uma pessoa");
+		return person;
+	}
+	
+	public void delete(String id) {
+		logger.info("Deletando um registro");
+	}
+
+	private Person mockPerson(int i) {
+		var person = new Person();
+		person.setNome("Rafael " + i);
+		person.setSobrenome("Ferraz " + i);
+		person.setGenero("Masculino " + i);
+		person.setEmail("teste@gmail.com " + i);
+		person.setEndereco("Avenida Nicolau Zarvos " + i);
+		return person;
+	}
 }
